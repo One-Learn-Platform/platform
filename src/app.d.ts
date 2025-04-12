3febd8180b25901a2cb493547f14da39fc42b68f
Rafli Malik <Rafli.Malik@gmail.com>
2025-04-12 14:42:16 +0700

    chore: update dependencies

--- src/app.d.ts
+++ src/app.d.ts
@@ -8,7 +8,7 @@ declare global {
 		interface Locals {
 			user: import("$lib/server/auth").SessionValidationResult["user"];
 			session: import("$lib/server/auth").SessionValidationResult["session"];
-			DB: D1Database;
+			DB: D1Database | undefined;
 		}
 		interface Platform {
 			env: {
