f00662c2abe6eed5a7c3898b93e4180c66200b1e
Rafli Malik <Rafli.Malik@gmail.com>
2025-04-12 18:39:42 +0700

    feat: add sidebar and super admin page

---
+++ src/lib/components/sidebar.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import Calendar from "@lucide/svelte/icons/calendar";
+	import House from "@lucide/svelte/icons/house";
+	import Inbox from "@lucide/svelte/icons/inbox";
+	import Search from "@lucide/svelte/icons/search";
+	import Settings from "@lucide/svelte/icons/settings";
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import { User, School, ChartArea } from "@lucide/svelte";
+
+	// Menu items.
+	const items = [
+		{
+			title: "Statistic",
+			url: "/statisitc",
+			icon: ChartArea,
+		},
+		{
+			title: "User",
+			url: "/user",
+			icon: User,
+		},
+		{
+			title: "School",
+			url: "/school",
+			icon: School,
+		},
+		{
+			title: "Search",
+			url: "/search",
+			icon: Search,
+		},
+		{
+			title: "Settings",
+			url: "/settings",
+			icon: Settings,
+		},
+	];
+</script>
+
+<Sidebar.Root variant="floating" collapsible="icon">
+	<Sidebar.Content>
+		<Sidebar.Group>
+			<Sidebar.GroupLabel>Management</Sidebar.GroupLabel>
+			<Sidebar.GroupContent>
+				<Sidebar.Menu>
+					{#each items as item (item.title)}
+						<Sidebar.MenuItem>
+							<Sidebar.MenuButton>
+								{#snippet child({ props })}
+									<a href={item.url} {...props}>
+										<item.icon />
+										<span>{item.title}</span>
+									</a>
+								{/snippet}
+							</Sidebar.MenuButton>
+						</Sidebar.MenuItem>
+					{/each}
+				</Sidebar.Menu>
+			</Sidebar.GroupContent>
+		</Sidebar.Group>
+	</Sidebar.Content>
+	<Sidebar.Trigger class="z-50 h-8 w-full" />
+</Sidebar.Root>
---
+++ src/lib/components/ui/sheet/sheet-content.svelte
@@ -0,0 +1,55 @@
+<script lang="ts" module>
+	import { tv, type VariantProps } from "tailwind-variants";
+	export const sheetVariants = tv({
+		base: "fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=closed]:duration-300 data-[state=closed]:animate-out data-[state=open]:duration-500 data-[state=open]:animate-in",
+		variants: {
+			side: {
+				top: "inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top",
+				bottom:
+					"inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
+				left: "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm",
+				right:
+					"inset-y-0 right-0 h-full w-3/4 border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
+			},
+		},
+		defaultVariants: {
+			side: "right",
+		},
+	});
+
+	export type Side = VariantProps<typeof sheetVariants>["side"];
+</script>
+
+<script lang="ts">
+	import { Dialog as SheetPrimitive, type WithoutChildrenOrChild } from "bits-ui";
+	import X from "@lucide/svelte/icons/x";
+	import type { Snippet } from "svelte";
+	import SheetOverlay from "./sheet-overlay.svelte";
+	import { cn } from "$lib/utils.js";
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		side = "right",
+		portalProps,
+		children,
+		...restProps
+	}: WithoutChildrenOrChild<SheetPrimitive.ContentProps> & {
+		portalProps?: SheetPrimitive.PortalProps;
+		side?: Side;
+		children: Snippet;
+	} = $props();
+</script>
+
+<SheetPrimitive.Portal {...portalProps}>
+	<SheetOverlay />
+	<SheetPrimitive.Content bind:ref class={cn(sheetVariants({ side }), className)} {...restProps}>
+		{@render children?.()}
+		<SheetPrimitive.Close
+			class="absolute top-4 right-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:ring-2 focus:ring-ring focus:ring-offset-2 focus:outline-none disabled:pointer-events-none data-[state=open]:bg-secondary"
+		>
+			<X class="size-4" />
+			<span class="sr-only">Close</span>
+		</SheetPrimitive.Close>
+	</SheetPrimitive.Content>
+</SheetPrimitive.Portal>
