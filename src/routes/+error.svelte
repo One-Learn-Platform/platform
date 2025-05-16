04fba24d1942466c618692f1780c3b419ef1d0ac
Rafli Malik <Rafli.Malik@gmail.com>
2025-05-16 23:14:02 +0700

    style: add custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { page } from "$app/state";
+
+	import { Button } from "$lib/components/ui/button/index.js";
+</script>
+
+<svelte:head>
+	<title>{page.status}{page.error?.message ? " - " + page.error.message : ""} | One Learn</title>
+</svelte:head>
+
+<main class="flex h-screen w-full flex-col items-center justify-center gap-4 pt-8">
+	<section class="space-y-1 text-center">
+		<h1 class="font-mono text-6xl font-black md:text-7xl lg:text-8xl xl:text-9xl">
+			{page.status}
+		</h1>
+		<p class="text-3  xl font-medium tracking-tight sm:text-4xl lg:text-5xl">{page.error?.message}</p>
+	</section>
+	<Button variant="outline" href="/" class="text-base">Home</Button>
+</main>
