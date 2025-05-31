221dcd65d75631e3e7d317ba12be036d9cb38d50
Rafli Malik <Rafli.Malik@gmail.com>
2025-05-31 13:19:17 +0700

    fix: redirect to login instead 404

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,5 +1,5 @@
 import type { LayoutServerLoad } from "./$types";
-import { error } from "@sveltejs/kit";
+import { redirect } from "@sveltejs/kit";
 
 export const load: LayoutServerLoad = async (event) => {
 	if (event.locals.user && (event.locals.user.role === 0 || event.locals.user.role === 1)) {
@@ -8,5 +8,5 @@ export const load: LayoutServerLoad = async (event) => {
 			session: event.locals.session,
 		};
 	}
-	return error(404, { message: "Not Found" });
+	return redirect(303, "/signin");
 };
