f900573660616cc4fc4f541af73480414af40d0b
Rafli Malik <Rafli.Malik@gmail.com>
2025-06-06 09:04:59 +0700

    fix: can't login because wrong validation

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -2,7 +2,7 @@ import type { LayoutServerLoad } from "./$types";
 import { redirect } from "@sveltejs/kit";
 
 export const load: LayoutServerLoad = async (event) => {
-	if (event.locals.user && (event.locals.user.role === 0 || event.locals.user.role === 1)) {
+	if (event.locals.user) {
 		return {
 			user: event.locals.user,
 			session: event.locals.session,
