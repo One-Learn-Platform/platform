5154d0bcdd1b777598255c476aef39bfd657381a
Rafli Malik <Rafli.Malik@gmail.com>
2025-06-06 09:06:01 +0700

    fix: can't open page

--- src/routes/(app)/subject/+page.server.ts
+++ src/routes/(app)/subject/+page.server.ts
@@ -5,7 +5,7 @@ import { getDb } from "$lib/server/db";
 import * as table from "$lib/schema/db";
 
 export const load: PageServerLoad = async (event) => {
-	if (event.locals.user && (event.locals.user.role === 0 || event.locals.user.role === 1)) {
+	if (event.locals.user) {
 		const db = getDb(event);
 		const subjectList = await db.select().from(table.subject);
 		return {
