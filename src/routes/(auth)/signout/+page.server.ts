04d7494808a403d358bee9fa3518927c7e979089
Rafli Malik <Rafli.Malik@gmail.com>
2025-06-13 13:55:48 +0700

    chore: delete session from db

--- src/routes/(auth)/signout/+page.server.ts
+++ src/routes/(auth)/signout/+page.server.ts
@@ -1,6 +1,10 @@
 import type { Actions, PageServerLoad } from "./$types";
 
+import { eq } from "drizzle-orm";
+
+import { session } from "$lib/schema/db";
 import * as auth from "$lib/server/auth";
+import { getDb } from "$lib/server/db";
 import { fail, redirect } from "@sveltejs/kit";
 
 export const load: PageServerLoad = async (event) => {
@@ -12,11 +16,13 @@ export const load: PageServerLoad = async (event) => {
 
 export const actions: Actions = {
 	signout: async (event) => {
+		const db = getDb(event);
 		if (!event.locals.session) {
 			return fail(401);
 		}
 		await auth.invalidateSession(event, event.locals.session.id);
 		auth.deleteSessionTokenCookie(event);
+		await db.delete(session).where(eq(session.id, event.locals.session.id));
 
 		return redirect(302, "/signin");
 	},
