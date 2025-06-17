085e91af41a78da35ba48091c8e62d3c63c0dde2
Rafli Malik <Rafli.Malik@gmail.com>
2025-06-18 00:36:51 +0700

    style: add styling for empty comment

--- src/routes/(app)/subject/[slug]/[page]/forum/[id]/+page.svelte
+++ src/routes/(app)/subject/[slug]/[page]/forum/[id]/+page.svelte
@@ -1,37 +1,38 @@
 <script lang="ts">
+	import { browser } from "$app/environment";
 	import { PUBLIC_R2_URL } from "$env/static/public";
 	import { onMount } from "svelte";
-	import type { PageServerData, ActionData } from "./$types";
-	import { browser } from "$app/environment";
 	import { flip } from "svelte/animate";
 	import { cubicOut } from "svelte/easing";
+	import type { ActionData, PageServerData } from "./$types";
 
 	import Dompurify from "dompurify";
 	import type { default as Quill } from "quill";
+	import { toast } from "svelte-sonner";
 	import { superForm } from "sveltekit-superforms";
 	import { zod4Client } from "sveltekit-superforms/adapters";
 	import { formSchema } from "./schema";
-	import { toast } from "svelte-sonner";
 
 	import dayjs from "dayjs";
 	import relativeTime from "dayjs/plugin/relativeTime";
 	dayjs.extend(relativeTime);
 
 	import * as Avatar from "$lib/components/ui/avatar/index.js";
+	import { Button } from "$lib/components/ui/button/index.js";
 	import * as Card from "$lib/components/ui/card/index.js";
 	import * as Form from "$lib/components/ui/form/index.js";
 	import { Input } from "$lib/components/ui/input/index.js";
-	import { Separator } from "$lib/components/ui/separator/index.js";
+	import { Label } from "$lib/components/ui/label/index.js";
 	import * as Pagination from "$lib/components/ui/pagination/index.js";
 	import * as Select from "$lib/components/ui/select/index.js";
-	import { Button } from "$lib/components/ui/button/index.js";
-	import { Label } from "$lib/components/ui/label/index.js";
+	import { Separator } from "$lib/components/ui/separator/index.js";
 
-	import { acronym } from "$lib/utils";
 	import { invalidateAll } from "$app/navigation";
+	import { acronym } from "$lib/utils";
 
 	import ArrowDown from "@lucide/svelte/icons/arrow-down";
 	import ArrowUp from "@lucide/svelte/icons/arrow-up";
+	import Wind from "@lucide/svelte/icons/wind";
 
 	let { data, form }: { data: PageServerData; form: ActionData } = $props();
 
@@ -199,112 +200,119 @@
 </form>
 <Separator />
 
-<div class="flex flex-row justify-between gap-2">
-	<div class="flex flex-row gap-2">
-		<Button variant="secondary" onclick={() => (sortOpt = sortOpt === "asc" ? "desc" : "asc")}>
-			{#if sortOpt === "asc"}
-				<ArrowDown />Descending
-			{:else}
-				<ArrowUp />Ascending
-			{/if}
-		</Button>
-		<Select.Root value={sortBy} type="single" onValueChange={(value) => (sortBy = value)}>
-			<Select.Trigger class="">{sortContent}</Select.Trigger>
-			<Select.Content>
-				<Select.Group>
-					<Select.Label>Sort By</Select.Label>
-					{#each sortChoice as option (option.value)}
-						<Select.Item value={option.value}>{option.label}</Select.Item>
-					{/each}
-				</Select.Group>
-			</Select.Content>
-		</Select.Root>
-	</div>
-	<div class="flex flex-row items-center gap-2">
-		<div class="flex flex-row gap-2">
-			<Label for="perPage">Comments per page:</Label>
-			<Input
-				name="perPage"
-				id="perPage"
-				type="number"
-				min="1"
-				max={data.commentsCount < 10 ? 10 : data.commentsCount}
-				bind:value={perPage}
-				class="w-16"
-				oninput={() => sessionStorage.setItem("forum_perPage", perPage.toString())}
-			/>
-		</div>
-		<Pagination.Root
-			count={data.commentsCount}
-			{perPage}
-			bind:page={currentPage}
-			class="mx-0 w-fit"
-			onPageChange={(page) => {
-				sessionStorage.setItem("forum_selectedPage", page.toString());
-			}}
-		>
-			{#snippet children({ pages, currentPage })}
-				<Pagination.Content>
-					<Pagination.Item>
-						<Pagination.PrevButton />
-					</Pagination.Item>
-					{#each pages as page (page.key)}
-						{#if page.type === "ellipsis"}
-							<Pagination.Item>
-								<Pagination.Ellipsis />
-							</Pagination.Item>
-						{:else}
-							<Pagination.Item>
-								<Pagination.Link {page} isActive={currentPage === page.value}>
-									{page.value}
-								</Pagination.Link>
-							</Pagination.Item>
-						{/if}
-					{/each}
-					<Pagination.Item>
-						<Pagination.NextButton />
-					</Pagination.Item>
-				</Pagination.Content>
-			{/snippet}
-		</Pagination.Root>
-	</div>
-</div>
-
 <div class="flex flex-col gap-4">
-	{#each commentGrouped[currentPage - 1] as comment (comment.id)}
-		{@const sanitizedDescription = browser ? Dompurify.sanitize(comment.content) : comment.content}
-		<div animate:flip={{ duration: 200, easing: cubicOut }}>
-			<Card.Root class="">
-				<Card.Content>
-					<div class="raw max-w-full **:break-words **:whitespace-break-spaces">
-						{@html sanitizedDescription}
-					</div>
-				</Card.Content>
-				<Separator />
-				<Card.Footer class="flex items-center gap-2">
-					<Avatar.Root>
-						<Avatar.Image src="{PUBLIC_R2_URL}/{comment.avatar}" alt={comment.fullname} />
-						<Avatar.Fallback>{acronym(comment.fullname ?? "ID")}</Avatar.Fallback>
-					</Avatar.Root>
-					<div class="flex w-full flex-row items-center justify-between gap-2">
-						<span class="truncate font-medium">
-							{comment.fullname}
-						</span>
-						<span class="text-sm text-muted-foreground">
-							{dateFormatter.format(new Date(comment.createdAt + "Z"))} | {dayjs(
-								comment.createdAt + "Z",
-							).fromNow()}
-						</span>
-					</div>
-				</Card.Footer>
-			</Card.Root>
+	{#if commentGrouped.length === 0}
+		<div class="flex flex-col items-center gap-1 self-center">
+			<Wind class="size-16  text-muted-foreground lg:size-24 xl:size-32" />
+			<p class="pt-2 pb-4 text-muted-foreground">No comments yet.</p>
 		</div>
-	{/each}
+	{:else}
+		<div class="flex flex-row justify-between gap-2">
+			<div class="flex flex-row gap-2">
+				<Button variant="secondary" onclick={() => (sortOpt = sortOpt === "asc" ? "desc" : "asc")}>
+					{#if sortOpt === "asc"}
+						<ArrowDown />Descending
+					{:else}
+						<ArrowUp />Ascending
+					{/if}
+				</Button>
+				<Select.Root value={sortBy} type="single" onValueChange={(value) => (sortBy = value)}>
+					<Select.Trigger class="">{sortContent}</Select.Trigger>
+					<Select.Content>
+						<Select.Group>
+							<Select.Label>Sort By</Select.Label>
+							{#each sortChoice as option (option.value)}
+								<Select.Item value={option.value}>{option.label}</Select.Item>
+							{/each}
+						</Select.Group>
+					</Select.Content>
+				</Select.Root>
+			</div>
+			<div class="flex flex-row items-center gap-2">
+				<div class="flex flex-row gap-2">
+					<Label for="perPage">Comments per page:</Label>
+					<Input
+						name="perPage"
+						id="perPage"
+						type="number"
+						min="1"
+						max={data.commentsCount < 10 ? 10 : data.commentsCount}
+						bind:value={perPage}
+						class="w-16"
+						oninput={() => sessionStorage.setItem("forum_perPage", perPage.toString())}
+					/>
+				</div>
+				<Pagination.Root
+					count={data.commentsCount}
+					{perPage}
+					bind:page={currentPage}
+					class="mx-0 w-fit"
+					onPageChange={(page) => {
+						sessionStorage.setItem("forum_selectedPage", page.toString());
+					}}
+				>
+					{#snippet children({ pages, currentPage })}
+						<Pagination.Content>
+							<Pagination.Item>
+								<Pagination.PrevButton />
+							</Pagination.Item>
+							{#each pages as page (page.key)}
+								{#if page.type === "ellipsis"}
+									<Pagination.Item>
+										<Pagination.Ellipsis />
+									</Pagination.Item>
+								{:else}
+									<Pagination.Item>
+										<Pagination.Link {page} isActive={currentPage === page.value}>
+											{page.value}
+										</Pagination.Link>
+									</Pagination.Item>
+								{/if}
+							{/each}
+							<Pagination.Item>
+								<Pagination.NextButton />
+							</Pagination.Item>
+						</Pagination.Content>
+					{/snippet}
+				</Pagination.Root>
+			</div>
+		</div>
+		{#each commentGrouped[currentPage - 1] as comment (comment.id)}
+			{@const sanitizedDescription = browser
+				? Dompurify.sanitize(comment.content)
+				: comment.content}
+			<div animate:flip={{ duration: 200, easing: cubicOut }}>
+				<Card.Root class="">
+					<Card.Content>
+						<div class="raw max-w-full **:break-words **:whitespace-break-spaces">
+							{@html sanitizedDescription}
+						</div>
+					</Card.Content>
+					<Separator />
+					<Card.Footer class="flex items-center gap-2">
+						<Avatar.Root>
+							<Avatar.Image src="{PUBLIC_R2_URL}/{comment.avatar}" alt={comment.fullname} />
+							<Avatar.Fallback>{acronym(comment.fullname ?? "ID")}</Avatar.Fallback>
+						</Avatar.Root>
+						<div class="flex w-full flex-row items-center justify-between gap-2">
+							<span class="truncate font-medium">
+								{comment.fullname}
+							</span>
+							<span class="text-sm text-muted-foreground">
+								{dateFormatter.format(new Date(comment.createdAt + "Z"))} | {dayjs(
+									comment.createdAt + "Z",
+								).fromNow()}
+							</span>
+						</div>
+					</Card.Footer>
+				</Card.Root>
+			</div>
+		{/each}
+		<p class="text-sm text-muted-foreground">
+			page {currentPage} of {Math.ceil(data.commentsCount / perPage)} from {data.commentsCount} data
+		</p>
+	{/if}
 </div>
-
-<p class="text-sm text-muted-foreground">
-	{currentPage} of {Math.ceil(data.commentsCount / perPage)} from {data.commentsCount} data
-</p>
 
 <style>
 	@reference "tailwindcss";
