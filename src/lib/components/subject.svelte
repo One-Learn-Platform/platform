ccd368cf9c748ac771073ffa5431c139c2c4805e
Rafli Malik <Rafli.Malik@gmail.com>
2025-06-17 10:38:41 +0700

    feat: adaptive icons

--- src/lib/components/subject.svelte
+++ src/lib/components/subject.svelte
@@ -6,6 +6,19 @@
 	import { subjectColor, subjectAbbreviation } from "$lib/functions/subject";
 
 	import Dna from "@lucide/svelte/icons/dna";
+	import Calculator from "@lucide/svelte/icons/calculator";
+	import UsersRound from "@lucide/svelte/icons/users-round";
+	import Microscope from "@lucide/svelte/icons/microscope";
+	import Shapes from "@lucide/svelte/icons/shapes";
+
+	const icons = $derived.by(() => {
+		if (subject.subjectTypeName.includes("Biology")) return Dna;
+		if (subject.subjectTypeName.includes("Mathematics")) return Calculator;
+		if (subject.subjectTypeName.includes("Science")) return Microscope;
+		if (subject.subjectTypeName.includes("History")) return UsersRound;
+		return Shapes;
+	});
+
 	type SubjectWithName = Subject & { subjectTypeName: string };
 	let { subject }: { subject: SubjectWithName } = $props();
 </script>
@@ -18,7 +31,7 @@
 	<Dna class="size-12 " />
 	<div>
 		<div class="flex items-center gap-2">
-			{subject.code}
+			{icons}
 			<Badge variant={subjectColor(subject.subjectTypeName)}>
 				{subjectAbbreviation(subject.subjectTypeName)}
 			</Badge>
