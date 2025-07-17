2c5c96295f52e1048ef5df90d98761261340be99
Rafli Malik <Rafli.Malik@gmail.com>
2025-07-18 00:36:45 +0700

    fix: remove i18n

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { i18n } from "$lib/i18n";
-	import { ParaglideJS } from "@inlang/paraglide-sveltekit";
 	import { ModeWatcher } from "mode-watcher";
 	import "@fontsource-variable/outfit";
 	import "@fontsource-variable/jetbrains-mono";
@@ -16,8 +14,6 @@
 	<link href="https://api.fontshare.com/v2/css?f[]=switzer@1,2&display=swap" rel="stylesheet" />
 </svelte:head>
 
-<ParaglideJS {i18n}>
-	<ModeWatcher />
-	<Toaster />
-	{@render children()}
-</ParaglideJS>
+<ModeWatcher />
+<Toaster />
+{@render children()}
