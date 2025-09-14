f9398b26bffa3903ca607f9408a0f0bd77dc9481
Rafli Malik <Rafli.Malik@gmail.com>
2025-09-14 15:31:58 +0700

    fix: prevent auth when prerendering

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,12 @@
+import { building } from "$app/environment";
+import * as auth from "$lib/server/auth.js";
 import type { Handle } from "@sveltejs/kit";
 import { sequence } from "@sveltejs/kit/hooks";
-import * as auth from "$lib/server/auth.js";
 
 const handleAuth: Handle = async ({ event, resolve }) => {
+	if (building) {
+		return resolve(event);
+	}
 	event.locals.DB = event.platform?.env.DB;
 	event.locals.R2 = event.platform?.env.MEDIA;
 	const sessionToken = event.cookies.get(auth.sessionCookieName);
